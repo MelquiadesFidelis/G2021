490aeafcb5c79dd236ab2e597844bc483fc228ff
Melquiades Fidelis <31939313+MelquiadesFidelis@users.noreply.github.com>
2021-02-07 16:03:31 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let fs = fullscreen();
+    fullscreen(!fs);
 }
 
 function draw() {
   background(0, 100, 200);
+  let fs = fullscreen();
+    fullscreen(!fs);
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
